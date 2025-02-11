ac2559e07e8d811e08b264f83c0185947f31dc3e
eyasuh <eyha12@gmail.com>
2025-02-10 23:28:22 -0500

    upgrade to spring data

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,9 +3,12 @@ CREATE TABLE IF NOT EXISTS Content (
   title varchar(255) not null,
     desc text,
     status Varchar(20) not null,
-    dontent_type varchar(50) not null,
+    content_type varchar(50) not null,
     date_created timestamp not null,
     date_updated timestamp,
     url varchar(255),
     primary key (id)
 );
+
+INSERT INTO Content(title, desc, status, content_type, date_created)
+VALUES ('My schema title', 'A schema post', 'IDEA', 'ARTICLE', CURRENT_TIME);
